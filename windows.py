6884a04d5056d7792775a941a93913d015b13e5b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-02 16:03:16 +0100

    Add files via upload

---
+++ windows.py
@@ -0,0 +1,25 @@
+#change next 2 lines
+winas="c:\\MinGW\\bin\\as.exe"
+winobj="c:\\MinGW\\bin\\objdump.exe"
+import os
+def spt(s):
+    r=s.split(" ")
+    rr=[]
+    for a in range(len(r)):
+        
+        if r[a].strip()!= "":
+            rr.append(r[a])
+    return rr
+print("give me the .o object file name ? ")
+a=input().strip()
+os.system(winobj+" -x "+a+" > out.txt")
+
+f1=open("out.txt","r")
+txt=f1.read()
+f1.close()
+txts=txt.split("\n")
+for a in txts:
+    aa=spt(a)
+    if len(aa)>0:
+        if aa[0].strip()=="architecture:":
+             print(aa[1])
